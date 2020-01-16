2d477943cba64fb6b87e1942e5162e36c3807ede
cam24USF <59966416+cam24USF@users.noreply.github.com>
2020-01-16 11:46:28 -0500

    Class 1
    Class one

---
+++ class meeting 1.R
@@ -0,0 +1,56 @@
+2+2
+3+4
+
+Number<- 6
+Number
+Number*2
+Number<-8
+Number*2
+c(1,2,3,4)
+times<-c(17,30,35,25,30,40,20)
+mean(times)
+time_hours <- times / 60
+mean(times)
+range(times)
+sqrt(times)
+times > 30
+times ==17
+
+
+which(times>30)
+any(times>30)
+all(times>30)
+?mean
+times[times>30]
+times[3]
+times[-3]
+times[3:5]
+times[c(2,4)]
+times[-c(2,4)]
+times[times>30]
+
+times
+times[1]
+times[1]<-47
+times
+times[times>30]<-NA
+times
+mean(times)
+mean(times,na.rm =TRUE )
+times[times>20&times<35]
+times>20
+times<35
+times>20 & times<35
+times[times>20 & times<35]
+mean(times,na.rm = True)
+mean(x=times)
+mean(times)
+
+
+
+
+mtcars
+?mtcars
+head(mtcars)
+str(mtcars)
+names(mtcars)
